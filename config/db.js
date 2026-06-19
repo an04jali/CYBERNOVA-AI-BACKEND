f111775f92185dfb7788953391799f383f471644
AARTI SINGH <147161209+an04jali@users.noreply.github.com>
2026-06-19 15:35:39 +0530

    fix mongo env variable name

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
-    console.log(process.env.MONGODB_URI);
+    console.log(process.env.MONGO_URI);
 
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB Connected");
   } catch (error) {
